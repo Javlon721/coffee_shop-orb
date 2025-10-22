b383b1bd4d368a19ae49a26d2ee12d07e820f7e3
Javlon Talipov <javlon.talipov13@mail.ru>
2025-10-22 10:47:00 +0500

    add created_at field to tables

--- sql/roles.sql
+++ sql/roles.sql
@@ -1,3 +1,5 @@
+DROP table if EXISTS users CASCADE;
+
 create table
   if not exists roles (
     role_id serial primary key,
--- sql/users_roles.sql
+++ sql/users_roles.sql
@@ -1,6 +1,9 @@
+DROP table if EXISTS users_roles;
+
 create table
   if not exists users_roles (
     id serial primary key,
     user_id int references users (user_id),
-    role_id int references roles (role_id)
+    role_id int references roles (role_id),
+    created_at timestamptz not null DEFAULT now ()
   )
