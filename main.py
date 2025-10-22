26728d50f93e0ae9ed5f3450a11a45fd4385dbb6
Javlon Talipov <javlon.talipov13@mail.ru>
2025-10-22 14:35:24 +0500

    add user & auth routers to app

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+from users.repository import UsersRepository
+from users.router import users_router
+from auth.router import auth_router
 
 app = FastAPI()
 
+
+app.include_router(auth_router)
+app.include_router(users_router)
+
+
 @app.get('/test')
 def test():
-  return "Hello Coffee Shop API"
+  return UsersRepository.get_user(1)
