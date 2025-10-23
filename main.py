1f628e99e934bcbaa473c13b21e416de0339f97d
Javlon Talipov <javlon.talipov13@mail.ru>
2025-10-23 08:40:01 +0500

    add roles and usersroles routers to app

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from fastapi import FastAPI
 from users.repository import UsersRepository
 from users.router import users_router
 from auth.router import auth_router
+from users_roles.router import users_roles_router
+from roles.router import roles_router
+
 
 app = FastAPI()
 
 
 app.include_router(auth_router)
 app.include_router(users_router)
+app.include_router(users_roles_router)
+app.include_router(roles_router)
 
 
 @app.get('/test')
