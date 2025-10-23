61c3c17c4ae4c38bca6a263b3ac0f3fa4042ebc4
Javlon Talipov <javlon.talipov13@mail.ru>
2025-10-23 08:38:58 +0500

    update sql

--- sql/users_roles.sql
+++ sql/users_roles.sql
@@ -5,5 +5,6 @@ create table
     id serial primary key,
     user_id int references users (user_id),
     role_id int references roles (role_id),
-    created_at timestamptz not null DEFAULT now ()
+    created_at timestamptz not null DEFAULT now (),
+    UNIQUE (user_id, role_id);
   )
