409fa291f101b3b15bf8afcbe583a9a834dbc1c8
Javlon Talipov <javlon.talipov13@mail.ru>
2025-10-25 12:27:42 +0500

    add repr dunder method for Base class

--- db/models.py
+++ db/models.py
@@ -27,7 +27,13 @@ class DB(ABC):
 
 
 class Base(DeclarativeBase):
-  pass
+
+  def __repr__(self):
+    cols = []
+    for col in self.__table__.columns.keys():
+        cols.append(f"{col}={getattr(self, col)}")
+
+    return f"<{self.__class__.__name__} {', '.join(cols)}>"
 
 
 INT_PK = Annotated[int, mapped_column(primary_key=True)]
