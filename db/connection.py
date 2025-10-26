b5e33f0f91720707c823a6a2efead54bb84031cd
Javlon Talipov <javlon.talipov13@mail.ru>
2025-10-26 20:58:58 +0500

    clean up db module

--- db/connection.py
+++ db/connection.py
@@ -2,41 +2,10 @@ from contextlib import _AsyncGeneratorContextManager, asynccontextmanager
 from typing import Annotated, Any, AsyncGenerator, Callable
 
 from fastapi import Depends
-from psycopg_pool import ConnectionPool
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncEngine, AsyncSession, AsyncConnection
 
 from db.config import DBConfig
-from db.models import DB, Base
-
-
-class _PsycopgDB(DB):
-  
-  def __init__(self) -> None:
-    self.pool = ConnectionPool(
-      DBConfig.URI,
-      max_size=DBConfig.CONNECTION_POOL_MAX_SIZE
-    )
-
-
-  def query(self, query: str, *args: Any) -> list[Any]:
-    with self.pool.connection() as conn:
-      result = conn.execute(query, args) # type: ignore
-      
-      return result.fetchall()
-
-
-  def query_one(self, query: str, *args: Any) -> Any: 
-    with self.pool.connection() as conn:
-      result = conn.execute(query, args) # type: ignore
-      
-      return result.fetchone()
-
-
-  def execute(self, query: str, *args: Any) -> Any: 
-    return self.query_one(query, *args)
-
-
-PsycopgDB = _PsycopgDB()
+from db.models import  Base
 
 
 class _ConnectionManager:
--- db/models.py
+++ db/models.py
@@ -1,29 +1,10 @@
 
-from abc import ABC, abstractmethod
-from typing import Any
 from datetime import datetime
 from typing import Annotated
 
 from sqlalchemy.orm import mapped_column
 from sqlalchemy import text
 from sqlalchemy.orm import DeclarativeBase
-
-
-class DB(ABC):
-  
-  @abstractmethod
-  def query(self, query: str, *args: Any) -> Any: 
-    raise NotImplemented
-
-
-  @abstractmethod
-  def query_one(self, query: str, *args: Any) -> Any: 
-    raise NotImplemented
-
-
-  @abstractmethod
-  def execute(self, query: str, *args: Any) -> Any: 
-    raise NotImplemented
 
 
 class Base(DeclarativeBase):
