7f5227c0ca7e6801a6ebce086cf73219064a48bb
Javlon Talipov <javlon.talipov13@mail.ru>
2025-10-27 11:40:26 +0500

    update docs

--- users/models.py
+++ users/models.py
@@ -56,6 +56,10 @@ class UsersORM(Base):
   """
   In some situations `relationship` from sqlalchemy is better choise,
   however i left with simple and brief `join`-usage structure of tables
+  
+  And about database indexes. As we know that postgres event scheduler smart enough 
+  to chose correct strategy for searching table's data, I ommited creation of email, or email_password, etc...
+  indexes. If we had huge database so i would prefere to chose indexes that can speed up serchings 
   """
   
   __tablename__ = "users"
